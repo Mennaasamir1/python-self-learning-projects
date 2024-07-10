3b1bda67b36aa7cc358ed4ba5ab199c56df4a8fe
Mennaasamir1 <monyy_20174@outlook.com>
2024-07-11 02:08:36 +0300

    task 2

---
+++ 1-Python-classes_objects/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a new square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.__size = size
+        
