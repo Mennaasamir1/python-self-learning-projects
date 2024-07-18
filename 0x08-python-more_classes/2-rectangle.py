af52cdfa9b4823291380c6df0fa2623b29089dbb
Mennaasamir1 <monyy_20174@outlook.com>
2024-07-18 21:38:32 +0300

    project completed

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+    
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer.")
+        
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        return self.__height
+    
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer.")
+        
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            self.perimeter = 0
+
+        return (2 * (self.height + self.width))
